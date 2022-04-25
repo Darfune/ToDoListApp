f55189c38a2d562d7488d7e7d5bb96221adc89fb
Darfune <harolep@gmail.com>
2022-04-25 20:29:37 +0300

    Search bar and sort option

--- app/src/main/java/com/example/todolistapp/fragments/list/RecyclerViewListFragment.kt
+++ app/src/main/java/com/example/todolistapp/fragments/list/RecyclerViewListFragment.kt
@@ -2,10 +2,8 @@ package com.example.todolistapp.fragments.list
 
 import android.os.Bundle
 import android.text.Layout
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.navigation.fragment.findNavController
 import com.example.todolistapp.R
@@ -22,14 +20,21 @@ class RecyclerViewListFragment : Fragment() {
 
         val floatingActionButton = view.findViewById<FloatingActionButton>(R.id.floatingActionButton)
         floatingActionButton.setOnClickListener{
-            findNavController().navigate(R.id.action_recyclerViewListFragment_to_addItemFragment2)
+            findNavController().navigate(R.id.action_recyclerViewListFragment_to_addItemFragment)
         }
 
-        val recyclerViewListLayout: View? =  view.findViewById(R.id.recyclerViewListLayout)
+        val recyclerViewListLayout: View? =  view.findViewById(R.id.recyclerViewListFragment)
         recyclerViewListLayout?.setOnClickListener {
-            findNavController().navigate(R.id.action_recyclerViewListFragment_to_updateFragment2)
+            findNavController().navigate(R.id.action_recyclerViewListFragment_to_updateFragment)
         }
+
+        setHasOptionsMenu(true)
+
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
     }
 
 }
