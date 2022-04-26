896150993f3d31ddd831024f9bfdcdcb2885a609
Darfune <harolep@gmail.com>
2022-04-26 12:52:32 +0300

    update fragment menu

--- app/src/main/java/com/example/todolistapp/fragments/add/AddItemFragment.kt
+++ app/src/main/java/com/example/todolistapp/fragments/add/AddItemFragment.kt
@@ -1,10 +1,8 @@
 package com.example.todolistapp.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.todolistapp.R
 
 class AddItemFragment : Fragment() {
@@ -14,7 +12,14 @@ class AddItemFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
+
+        setHasOptionsMenu(true)
+
         return inflater.inflate(R.layout.fragment_add_item, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 
 }
