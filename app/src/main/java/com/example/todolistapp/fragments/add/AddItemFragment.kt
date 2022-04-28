260c1b677e0fe48e9769a7180a8f4bf77fd97af5
Darfune <harolep@gmail.com>
2022-04-28 19:16:08 +0300

    Added Binding

--- app/src/main/java/com/example/todolistapp/fragments/add/AddItemFragment.kt
+++ app/src/main/java/com/example/todolistapp/fragments/add/AddItemFragment.kt
@@ -1,19 +1,20 @@
 package com.example.todolistapp.fragments.add
 
 import android.os.Bundle
-import android.text.TextUtils
 import android.view.*
+import android.view.View.inflate
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
 import androidx.navigation.fragment.findNavController
 import com.example.todolistapp.R
-import com.example.todolistapp.data.models.Priority
 import com.example.todolistapp.data.models.ToDoData
 import com.example.todolistapp.data.viewmodel.ToDoViewModel
+import com.example.todolistapp.databinding.ActivityMainBinding
+import com.example.todolistapp.databinding.ActivityMainBinding.inflate
+import com.example.todolistapp.databinding.FragmentAddItemBinding
 import com.example.todolistapp.fragments.SharedViewModel
 
 class AddItemFragment : Fragment() {
@@ -22,26 +23,20 @@ class AddItemFragment : Fragment() {
     private val toDoViewModel: ToDoViewModel by viewModels()
     private val sharedViewModel: SharedViewModel by viewModels()
 
-    private var titleEditText: EditText? = null
-    private var prioritySpinner: Spinner? = null
-    private var descriptionEditText: EditText? = null
+    private lateinit var binding: FragmentAddItemBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_add_item, container, false)
-
-        titleEditText = view.findViewById<EditText>(R.id.titleEditText)
-        prioritySpinner = view.findViewById<Spinner>(R.id.prioritySpinner)
-        descriptionEditText = view.findViewById<EditText>(R.id.descriptionEditText)
-
+        binding = FragmentAddItemBinding.inflate(layoutInflater,container, false)
         setHasOptionsMenu(true)
-        
-        prioritySpinner?.onItemSelectedListener = sharedViewModel.listener
 
-        return view
+        binding.prioritySpinner.onItemSelectedListener = sharedViewModel.listener
+
+        return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -56,9 +51,9 @@ class AddItemFragment : Fragment() {
     }
 
     private fun insertDataToDatabase() {
-        val title = titleEditText?.text.toString()
-        val priority = prioritySpinner?.selectedItem.toString()
-        val description = descriptionEditText?.text.toString()
+        val title = binding.titleEditText.text.toString()
+        val priority = binding.prioritySpinner.selectedItem.toString()
+        val description = binding.descriptionEditText.text.toString()
 
         val validation = sharedViewModel.verifyDataFromUser(title,description)
 
